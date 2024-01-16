f310d66d73600f6fc61ae783fd61bbb04e29995b
mrizkisaputra <mrizkisaputra6@gmail.com>
2024-01-16 23:18:57 +0700

    starter project

---
+++ src/main/java/com/mrizkisaputra/BelajarSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mrizkisaputra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringSecurityApplication.class, args);
+	}
+
+}
